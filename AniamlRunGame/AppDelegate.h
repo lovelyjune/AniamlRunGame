66fd6b9a1d84f22eee8bf9a79f09077ba24d92e0
yingxin ye <yingxinye@yingxindeiMac.local>
2017-07-15 16:38:01 +0800

    Initial Commit

---
+++ AniamlRunGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AniamlRunGame
+//
+//  Created by yingxin ye on 2017/7/15.
+//  Copyright © 2017年 yingxin ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AniamlRunGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AniamlRunGame
+//
+//  Created by yingxin ye on 2017/7/15.
+//  Copyright © 2017年 yingxin ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
